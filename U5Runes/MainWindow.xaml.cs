2c9a109efa0b01584e96f7810c6214c5a54ba89a
Richard Todd <rwtodd@users.noreply.github.com>
2018-10-09 15:23:34 -0500

    Basic text updating functionality works.
    The code is modelled after the lua original. The biggest difficulty is that
    C#'s Substring isn't forgiving about indices outside the string.  Otherwise,
    it was straightforward.

--- U5Runes/MainWindow.xaml.cs
+++ U5Runes/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace U5Runes
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (RuneBox == null) return;
+
+            Style lineStyle = (Style)FindResource("CenteredText");
+
+            // STEP 1: Clear the existing glyphs
+            RuneBox.Children.Clear();
+
+            // STEP 2: Go line by line... looking up runes
+            foreach(var line in English.Text.Split(new[] {'\r','\n'}))
+            {
+                var transformed = line.ToLower().Trim();
+                var len = transformed.Length;
+
+                var linePanel = new StackPanel { Style=lineStyle };
+                var idx = 1;
+                while (idx <= len) 
+                {
+                    var rune = $"Rune-{transformed.Substring(idx-1,(idx < len) ? 2 : 1)}";
+                    Style letter = (Style)TryFindResource(rune);
+                    if(letter == null)
+                    {
+                        letter = (Style)TryFindResource(rune.Substring(0,6));
+                        idx += 1;
+                    } else
+                    {
+                        idx += 2;
+                    }
+                    if(letter != null)
+                    {
+                      linePanel.Children.Add(new Control { Style = letter });
+                    }
+                }
+
+                if(linePanel.Children.Count > 0)
+                    RuneBox.Children.Add(linePanel);
+            }
+        }
     }
 }
