623fb425accea85c423ef5badb522b37937b7498
Richard Todd <rwtodd@users.noreply.github.com>
2019-08-11 20:23:00 -0500

    Finished the rune glyphs, refined spacing

--- src/org.rwtodd.u5tools.runes/classes/org/rwtodd/u5tools/runes/Alphabet.java
+++ src/org.rwtodd.u5tools.runes/classes/org/rwtodd/u5tools/runes/Alphabet.java
@@ -18,139 +18,357 @@
 package org.rwtodd.u5tools.runes;
 
 import java.awt.geom.Path2D;
-import java.awt.Shape;
 import java.util.HashMap;
 
-final class Alphabet extends HashMap<String, Shape> {
+final class Alphabet extends HashMap<String, Alphabet.Rune> {
+
+    class Rune extends Path2D.Float {
+
+        private final float pathWidth;
+
+        Rune(float width) {
+            super();
+            pathWidth = width;
+        }
+
+        Rune() {
+            this(5.5f);
+        }
+
+        float getRuneWidth() {
+            return pathWidth;
+        }
+
+        float getRuneHeight() {
+            return 10f;
+        }
+    }
 
     private final double letterHeight;
     private final double letterWidth;
-    
+
     Alphabet() {
         super(26);
-        
-        var s = new Path2D.Float();
-        s.moveTo(0.0f, 10.0f); s.lineTo(0.0f, 0.0f); s.lineTo(5.0f,5.0f);
-        s.moveTo(0.0f, 3.0f); s.lineTo(4.0f, 7.0f);
+
+        var s = new Rune();
+        s.moveTo(0.0f, 10.0f);
+        s.lineTo(0.0f, 0.0f);
+        s.lineTo(5.0f, 5.0f);
+        s.moveTo(0.0f, 3.0f);
+        s.lineTo(4.0f, 7.0f);
         put("A", s);
 
-        s = new Path2D.Float();
-        s.moveTo(0f,10f); s.lineTo(0f,0f); s.lineTo(3.5f,3f); 
-        s.lineTo(2.5f,5f); s.lineTo(5f,7f); s.lineTo(0f,10f);
-        s.lineTo(0f,0f);
+        s = new Rune();
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.lineTo(3.5f, 3f);
+        s.lineTo(2.5f, 5f);
+        s.lineTo(5f, 7f);
+        s.lineTo(0f, 10f);
+        s.lineTo(0f, 0f);
         put("B", s);
-        
-        s = new Path2D.Float();
-        s.moveTo(0f,10f); s.lineTo(1.5f,0f);
-        s.moveTo(0.9f,4.8f); s.lineTo(5f,10f);
+
+        s = new Rune();
+        s.moveTo(0f, 10f);
+        s.lineTo(1.5f, 0f);
+        s.moveTo(0.9f, 4.8f);
+        s.lineTo(5f, 10f);
         put("C", s);
-        
-        s = new Path2D.Float();
-        s.moveTo(0f,0f); s.lineTo(5f,10f); s.lineTo(5f,0f); 
-        s.lineTo(0f,10f); s.lineTo(0f,0f); s.lineTo(5f,10f);
+
+        s = new Rune(6f);
+        s.moveTo(0f, 0f);
+        s.lineTo(5f, 10f);
+        s.lineTo(5f, 0f);
+        s.lineTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.lineTo(5f, 10f);
         put("D", s);
-        
-        s = new Path2D.Float();
-        s.moveTo(0f,10f); s.lineTo(0.7f,0f); s.lineTo(2.5f,4f); 
-        s.lineTo(4.3f,0f); s.lineTo(5f,10f);
+
+        s = new Rune();
+        s.moveTo(0f, 10f);
+        s.lineTo(0.7f, 0f);
+        s.lineTo(2.5f, 4f);
+        s.lineTo(4.3f, 0f);
+        s.lineTo(5f, 10f);
         put("E", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.moveTo(0f, 6.5f);
+        s.lineTo(3f, 6.5f);
+        s.lineTo(5f, 4.5f);
+        s.lineTo(5f, 0f);
+        s.moveTo(0f, 4f);
+        s.lineTo(1.5f, 4f);
+        s.lineTo(2.5f, 3f);
+        s.lineTo(2.5f, 0f);
         put("F", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//g
+        s.moveTo(0f, 10f);
+        s.lineTo(5f, 0f);
+        s.moveTo(0f, 0f);
+        s.lineTo(5f, 10f);
         put("G", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune(4.5f);
+//h 
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.lineTo(4.5f, 3f);
+        s.lineTo(0f, 7f);
         put("H", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune(1.0f);
+//rune-i
+        s.moveTo(0f, 0f);
+        s.lineTo(0f, 10f);
         put("I", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-j
+        s.moveTo(2.5f, 10f);
+        s.lineTo(2.5f, 0f);
+        s.moveTo(2.5f, 0f);
+        s.lineTo(5f, 3f);
+        s.lineTo(2.5f, 6.5f);
+        s.lineTo(0f, 3f);
+        s.lineTo(2.5f, 0f);
+        s.lineTo(5f, 3f);
         put("J", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-k
+        s.moveTo(0f, 10f);
+        s.lineTo(1.5f, 0f);
+        s.moveTo(1.1f, 4.8f);
+        s.lineTo(3f, 5.5f);
+        s.lineTo(5f, 10f);
         put("K", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune(3.5f);
+//rune-l
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.lineTo(3f, 5f);
         put("L", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-m
+        s.moveTo(0f, 10f);
+        s.lineTo(0.7f, 0f);
+        s.lineTo(4.7f, 7f);
+        s.moveTo(5f, 10f);
+        s.lineTo(4.3f, 0f);
+        s.lineTo(0.3f, 7f);
         put("M", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-n
+        s.moveTo(2.5f, 10f);
+        s.lineTo(2.5f, 0f);
+        s.moveTo(0f, 4f);
+        s.lineTo(5f, 7f);
         put("N", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-o
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.lineTo(2.5f, 3f);
+        s.lineTo(5f, 0f);
+        s.moveTo(0f, 3f);
+        s.lineTo(2.5f, 6f);
+        s.lineTo(5f, 3f);
         put("O", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-p
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.moveTo(0f, 4.5f);
+        s.lineTo(2.5f, 4.5f);
+        s.lineTo(3.5f, 3.5f);
+        s.lineTo(5f, 0f);
+        s.moveTo(0f, 6f);
+        s.lineTo(2.5f, 6.5f);
+        s.lineTo(3.5f, 7.5f);
+        s.lineTo(5f, 10f);
         put("P", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-q
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.moveTo(0f, 4.5f);
+        s.lineTo(2.5f, 4.5f);
+        s.lineTo(3.5f, 3.5f);
+        s.lineTo(5f, 0f);
         put("Q", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-r
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.lineTo(4.2f, 3.5f);
+        s.lineTo(1.5f, 6f);
+        s.lineTo(5f, 10f);
         put("R", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-s
+        s.moveTo(0f, 0f);
+        s.lineTo(0f, 6f);
+        s.lineTo(5f, 3.5f);
+        s.lineTo(5f, 10f);
         put("S", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-t
+        s.moveTo(2.5f, 0f);
+        s.lineTo(2.5f, 10f);
+        s.moveTo(0f, 3.5f);
+        s.lineTo(2.5f, 0f);
+        s.lineTo(5f, 3.5f);
         put("T", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-u
+        s.moveTo(0f, 10f);
+        s.lineTo(0.7f, 0f);
+        s.lineTo(4.5f, 3.5f);
+        s.lineTo(5f, 10f);
         put("U", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-v
+        s.moveTo(0f, 10f);
+        s.lineTo(2.5f, 0f);
+        s.lineTo(5f, 10f);
         put("V", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune(6f);
+//rune-w
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.moveTo(5f, 10f);
+        s.lineTo(5f, 0f);
+        s.moveTo(0f, 2f);
+        s.lineTo(5f, 6f);
+        s.moveTo(0f, 4f);
+        s.lineTo(5f, 8f);
         put("W", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-x
+        s.moveTo(2.5f, 0f);
+        s.lineTo(2.5f, 10f);
+        s.moveTo(0f, 10f);
+        s.lineTo(2.5f, 7f);
+        s.lineTo(5f, 10f);
         put("X", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-y
+        s.moveTo(0f, 10f);
+        s.lineTo(0.7f, 0f);
+        s.lineTo(4.5f, 3.5f);
+        s.lineTo(5f, 10f);
+        s.moveTo(0.2f, 3f);
+        s.lineTo(4.8f, 6f);
+        s.moveTo(2.5f, 4.5f);
+        s.lineTo(2.5f, 10f);
         put("Y", s);
-        
-        s = new Path2D.Float();
+
+        s = new Rune();
+//rune-z
+        s.moveTo(2.5f, 0f);
+        s.lineTo(2.5f, 10f);
+        s.moveTo(0f, 0f);
+        s.lineTo(2.5f, 4.5f);
+        s.lineTo(5f, 0f);
         put("Z", s);
-        
-        s = new Path2D.Float();
-        s.moveTo(2f,5f); s.lineTo(2.5f,4f); s.lineTo(3f,5f); 
-        s.lineTo(2.5f,6f); s.lineTo(2f,5f); s.lineTo(2.5f,4f);
+
+        s = new Rune(5f);
+        s.moveTo(2f, 5f);
+        s.lineTo(2.5f, 4f);
+        s.lineTo(3f, 5f);
+        s.lineTo(2.5f, 6f);
+        s.lineTo(2f, 5f);
+        s.lineTo(2.5f, 4f);
         put("space", s);
 
-        s = new Path2D.Float();
+        s = new Rune(4.5f);
+//rune-th
+        s.moveTo(0f, 10f);
+        s.lineTo(0f, 0f);
+        s.moveTo(0f, 2.5f);
+        s.lineTo(4.5f, 5f);
+        s.lineTo(0f, 7.5f);
         put("TH", s);
 
-        s = new Path2D.Float();
+        s = new Rune();
+//rune-ng
+        s.moveTo(0f, 10f);
+        s.lineTo(4.5f, 4f);
+        s.lineTo(2.5f, 0f);
+        s.lineTo(0f, 4f);
+        s.lineTo(4.5f, 10f);
         put("NG", s);
 
-        s = new Path2D.Float();
+        s = new Rune(7f);
+//rune-ea
+        s.moveTo(3.5f, 0f);
+        s.lineTo(3.5f, 10f);
+        s.moveTo(0f, 0f);
+        s.lineTo(1.75f, 3.5f);
+        s.lineTo(3.5f, 0f);
+        s.lineTo(5.25f, 3.5f);
+        s.lineTo(7f, 0f);
         put("EA", s);
 
-        s = new Path2D.Float();
+        s = new Rune(6f);
+//rune-st
+        s.moveTo(0f, 0f);
+        s.lineTo(0f, 10f);
+        s.lineTo(2.5f, 7f);
+        s.lineTo(5f, 10f);
+        s.lineTo(5f, 0f);
+        s.lineTo(2.5f, 3f);
+        s.lineTo(0f, 0f);
+        s.lineTo(0f, 8f);
         put("ST", s);
 
-        s = new Path2D.Float();
+        s = new Rune();
+//rune-ee
+        s.moveTo(0f, 10f);
+        s.lineTo(5f, 5f);
+        s.lineTo(0f, 0f);
+        s.moveTo(5f, 10f);
+        s.lineTo(0f, 5f);
+        s.lineTo(5f, 0f);
         put("EE", s);
-        
+
         // calculate the max letter height...
         letterHeight = values().stream()
-                .mapToDouble((l) -> l.getBounds2D().getHeight())
+                .mapToDouble((l) -> l.getRuneHeight())
                 .max()
                 .getAsDouble();
+
         // calculate the max letter width...
         letterWidth = values().stream()
-                .mapToDouble((l) -> l.getBounds2D().getWidth())
+                .mapToDouble((l) -> l.getRuneWidth())
                 .max()
                 .getAsDouble();
     }
 
-    double getLetterHeight() { return letterHeight; }
-    double getLetterWidth() { return letterWidth; }
-}
+    double getLetterHeight() {
+        return letterHeight;
+    }
 
+    double getLetterWidth() {
+        return letterWidth;
+    }
+}
--- src/org.rwtodd.u5tools.runes/classes/org/rwtodd/u5tools/runes/RunePanel.java
+++ src/org.rwtodd.u5tools.runes/classes/org/rwtodd/u5tools/runes/RunePanel.java
@@ -18,6 +18,7 @@
 package org.rwtodd.u5tools.runes;
 
 import java.awt.AlphaComposite;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -46,139 +47,163 @@ class RunePanel extends JPanel {
     private double shadowOffsetY;
     private int paddingX;
     private int paddingY;
-    
+
     RunePanel() {
         super();
         letters = new Path2D.Float();
-        spacingX = 1f;
-        spacingY = 1f;
+        spacingX = 2f;
+        spacingY = 2f;
         alphabet = new Alphabet();
         setForeground(Color.ORANGE);
         setBackground(Color.GREEN);
         shadowColor = Color.BLACK;
-        shadowOffsetX = 1.0;
-        shadowOffsetY = 1.0;
-        paddingX = (int)alphabet.getLetterWidth();
-        paddingY = (int)alphabet.getLetterHeight();
+        shadowOffsetX = 0.5;
+        shadowOffsetY = 0.5;
+        paddingX = (int) alphabet.getLetterWidth();
+        paddingY = (int) alphabet.getLetterHeight();
     }
 
-    /** converts a line of text into a composite Shape.
-     * 
+    /**
+     * converts a line of text into a composite Shape.
+     *
      * @param line the text to convert
      * @return a Shape of runes for the line
      */
     private Shape lineToShape(String line) {
         final var space = alphabet.get("space");
         final var result = new Path2D.Float();
-        Shape lastUsedGlyph = space;
-        
+        boolean spacePending = false;
+
         line = line.toUpperCase();
         final var llen = line.length();
-   
+
         int idx = 0;
         double xlevel = 0;
-        while(idx < llen) {
+        while (idx < llen) {
             // try the next two chars, then just the next char.. is it a rune?
-            final var glyphStr = line.substring(idx,Math.min(llen,idx+2));
+            final var glyphStr = line.substring(idx, Math.min(llen, idx + 2));
             var glyph = alphabet.get(glyphStr);
-            if(glyph == null) {
-                glyph = alphabet.get(glyphStr.substring(0,1));
-                if(glyph == null) glyph = space;
+            if (glyph == null) {
+                glyph = alphabet.get(glyphStr.substring(0, 1));
+                if (glyph == null) {
+                    glyph = space;
+                }
                 ++idx;
             } else {
                 idx += 2;
             }
-            
-            // if this is a space, and the last glyph was a space, skip it..
-            if((glyph == space) && (lastUsedGlyph == space)) continue;
-            
+
+            // if this is a space, and not at the beginning of the line,
+            // just remember it.
+            if (glyph == space) {
+                if (xlevel > 0.0) {
+                    spacePending = true;
+                }
+                continue;
+            }
+
             // ok, add the glyph...
-            lastUsedGlyph = glyph;
+            if (spacePending) {
+                spacePending = false;
+                result.append(
+                        space.getPathIterator(
+                                AffineTransform.getTranslateInstance(xlevel, 0.0)),
+                        false);
+                xlevel += space.getRuneWidth() + spacingX;
+            }
+
             result.append(
                     glyph.getPathIterator(
                             AffineTransform.getTranslateInstance(xlevel, 0.0)),
                     false);
-            xlevel += glyph.getBounds2D().getWidth() + spacingX;
+            xlevel += glyph.getRuneWidth() + spacingX;
         }
-        
+
         return result;
     }
 
-    /** render a multi-line string to runes. This method converts all of the
-     * text into a large composite 
-     * {@code Path2D} that contains the runes.
-     * 
+    /**
+     * render a multi-line string to runes. This method converts all of the text
+     * into a large composite {@code Path2D} that contains the runes.
+     *
      * @param txt the lines of text
      * @return the rendered shape
      */
     Shape render(String txt) {
         Shape[] lines = txt.lines().map(this::lineToShape).toArray(Shape[]::new);
         final double maxWidth = Arrays.stream(lines)
-                .mapToDouble((s) -> s.getBounds2D().getWidth())
+                .mapToDouble((s) -> s.getBounds2D().getMaxX())
                 .max()
                 .orElse(1.0);
-
         final var answer = new Path2D.Float();
         double ylevel = 0.0;
         for (final var s : lines) {
             answer.append(s.getPathIterator(
                     AffineTransform.getTranslateInstance(
-                            (maxWidth - s.getBounds2D().getWidth()) / 2.0,
+                            (maxWidth - s.getBounds2D().getMaxX()) / 2.0,
                             ylevel)),
                     false);
             ylevel += alphabet.getLetterHeight() + spacingY;
         }
         return answer;
     }
-    
-    /** renders given text as runes, and redraws the panel.
-     * 
-     * @param txt the text to render 
+
+    /**
+     * renders given text as runes, and redraws the panel.
+     *
+     * @param txt the text to render
      */
     void drawText(String txt) {
         letters = render(txt);
         repaint();
     }
-    
+
     @Override
     protected void paintComponent(Graphics g) {
-          final var shapeSize = letters.getBounds2D();
-          final var x = getWidth();
-          final var y = getHeight();
-          final var xscale = x / (shapeSize.getWidth() + paddingX);
-          final var yscale = y / (shapeSize.getHeight() + paddingY);
-          
-          var img = getGraphicsConfiguration()
-                  .createCompatibleImage(x,y,Transparency.TRANSLUCENT);
-          var g2 = (Graphics2D)img.getGraphics();
-          g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                  RenderingHints.VALUE_ANTIALIAS_ON);
-          g2.scale(xscale,yscale);
+        final var shapeSize = letters.getBounds2D();
+        final var panelX = getWidth();
+        final var panelY = getHeight();
+        final var shapeX = shapeSize.getMaxX();
+        final var shapeY = shapeSize.getMaxY();
+        final var scaleFactor = Math.min(
+                panelX / (shapeX + paddingX),
+                panelY / (shapeY + paddingY)); // maintain aspect ratio
 
-          g2.setColor(getForeground());
-          g2.translate(paddingX/2.0, paddingY/2.0);
-          g2.draw(letters);
-          g2.dispose();
+        var img = getGraphicsConfiguration()
+                .createCompatibleImage(panelX, panelY, Transparency.TRANSLUCENT);
+        var g2 = (Graphics2D) img.getGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.translate(
+                (panelX - shapeX * scaleFactor) / 2.0,
+                (panelY - shapeY * scaleFactor) / 2.0);
+        g2.scale(scaleFactor, scaleFactor);
+        g2.setColor(getForeground());
+        g2.setStroke(new BasicStroke(1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL));
+        g2.draw(letters);
+        g2.dispose();
 
-          // now blur...
-          float[] matrix = new float[36];
-          for (int i = 0; i < 36; i++)  matrix[i] = 1.0f/36.0f;
-          var blur = new java.awt.image.ConvolveOp( new java.awt.image.Kernel(6, 6, matrix), java.awt.image.ConvolveOp.EDGE_ZERO_FILL, null );
-          var blurred = blur.filter(img, null);
-          g2 = (Graphics2D)blurred.getGraphics();
-          g2.setColor(shadowColor);
-          g2.setComposite(AlphaComposite.SrcIn);
-          g2.fillRect(0,0,x,y);
-          g2.dispose();
+        // now blur...
+        float[] matrix = new float[36];
+        for (int i = 0; i < 36; i++) {
+            matrix[i] = 1.0f / 36.0f;
+        }
+        var blur = new java.awt.image.ConvolveOp(new java.awt.image.Kernel(6, 6, matrix), java.awt.image.ConvolveOp.EDGE_ZERO_FILL, null);
+        var blurred = blur.filter(img, null);
+        g2 = (Graphics2D) blurred.getGraphics();
+        g2.setColor(shadowColor);
+        g2.setComposite(AlphaComposite.SrcIn);
+        g2.fillRect(0, 0, panelX, panelY);
+        g2.dispose();
 
-          // now paint the background, shadow, and image
-          g.setColor(getBackground());
-          g.fillRect(0,0,x,y);
-          g.drawImage(blurred,
-                  (int)(shadowOffsetX*xscale),
-                  (int)(shadowOffsetY*yscale),
-                  null);
-          g.drawImage(img,0,0,null);
+        // now paint the background, shadow, and image
+        g.setColor(getBackground());
+        g.fillRect(0, 0, panelX, panelY);
+        g.drawImage(blurred,
+                (int) (shadowOffsetX * scaleFactor),
+                (int) (shadowOffsetY * scaleFactor),
+                null);
+        g.drawImage(img, 0, 0, null);
     }
-    
+
 }
