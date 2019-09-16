7374aa568c5a263bd9f7dd0785e3e0682ebfd396
Richard Todd <rwtodd@users.noreply.github.com>
2019-09-15 20:44:32 -0500

    changed previewpane on the color picker

--- org.rwtodd.u5tools.runes/src/main/java/org/rwtodd/u5tools/runes/ColorButton.java
+++ org.rwtodd.u5tools.runes/src/main/java/org/rwtodd/u5tools/runes/ColorButton.java
@@ -7,27 +7,58 @@ package org.rwtodd.u5tools.runes;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 /**
  * a button that displays a color, and activates a color chooser.
+ *
  * @author Richard Todd
  */
-public final class ColorButton extends JButton {
+public final class ColorButton extends JButton implements ActionListener {
 
     private Color current;
+    private final JColorChooser colorChooser;
+    private final JDialog dialog;
+
+    private static class PreviewLabel extends JPanel {
+
+        PreviewLabel() {
+            super();
+            setBorder(javax.swing.BorderFactory.createEmptyBorder(15, 40, 15, 40));
+        }
+        
+        @Override
+        public void setForeground(Color c) {
+            super.setForeground(c);
+            setBackground(c);
+        }
+    }
 
     public ColorButton(Color c) {
+        super();
         setSelectedColor(c);
+        colorChooser = new JColorChooser(current);
+        colorChooser.setPreviewPanel(new PreviewLabel());
+        dialog = JColorChooser.createDialog(this, "Choose a color", true, colorChooser, this, null);
+
         super.addActionListener((e) -> {
-            Color newColor = JColorChooser.showDialog(null, "Choose a color", current);
-            setSelectedColor(newColor);
+            colorChooser.setColor(current);
+            dialog.setVisible(true);
         });
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setSelectedColor(colorChooser.getColor());
+    }
+    
     public Color getSelectedColor() {
         return current;
     }
